1a0139bce1519e46a901fbf58822959dfee9ea35
sagarFreshcodes <sagar.vavadiya@freshcodes.in>
2023-12-12 23:13:16 +0530

    ADD: update relationship functionality

--- src/controller/General_controller/attribute_crud.ts
+++ src/controller/General_controller/attribute_crud.ts
@@ -22,7 +22,7 @@ export const Get_attribute = async (req: Request, res: Response) => {
   try {
     const ExtractFilterArray = ExtractFilterArrayWithKey(req.body)
     const filterValue = ReturnFilterValue(ExtractFilterArray)
-    GetRecord(attributeRepo, res, Attribute, objectForAdd, messageData.ATTRIBUTE_ADD_SUCCESSFULL, { isFilter: Object.keys(ExtractFilterArray).length != 0, filterValue: filterValue, filterData: ExtractFilterArray, modelName:"Attribute" },)
+    GetRecord(attributeRepo, res, Attribute, objectForAdd, messageData.ATTRIBUTE_ADD_SUCCESSFULL, { isFilter: Object.keys(ExtractFilterArray).length != 0, filterValue: filterValue, filterData: ExtractFilterArray, modelName: "Attribute" },)
   } catch (error) {
     ErrorResponce(res, error, messageData.UNKNOWN)
   }
@@ -37,15 +37,14 @@ export const Add_attribute = async (req: Request, res: Response) => {
     ];
     const objectForAdd = ObjectWithRequireKeysValue(req.body, keysArray)
     let attribute: any = new Attribute();
-    attribute = { ...attribute, ...objectForAdd } 
+    attribute = { ...attribute, ...objectForAdd }
     const relativeRepo = AppDataSource.getRepository(Category)
     const relateIds: number[] | never[] = extractNumbersFromString(category_id)
     const relativeField = "categories"
-    const RelationOption: RelationOptionSchema = { isRelation: relateIds.length>0, relativeRepo, relateIds, relativeField }
+    const RelationOption: RelationOptionSchema = { isRelation: relateIds.length > 0, relativeRepo, relateIds, relativeField }
     AddRecord(attributeRepo, attribute, res, messageData.ATTRIBUTE_ADD_SUCCESSFULL, RelationOption, {})
   } catch (error) {
-    console.log("error=============",error);
-    
+
     ErrorResponce(res, error, messageData.UNKNOWN)
   }
 }
@@ -53,10 +52,8 @@ export const Add_attribute = async (req: Request, res: Response) => {
 export const Edit_attribute = async (req: Request, res: Response) => {
   try {
     const { id, category_id } = req.body
-    const keysToExtract = ['name', "is_field", "is_require", "category_id", 'status'];
-    const objectForUpadate = ExtractKeys(req.body, keysToExtract);
-
-
+    const keysToExtract = ['name', "is_field", "is_require", 'status'];
+    const objectForUpadate = ExtractKeys(req.body, keysToExtract); 
     const relativeRepo = AppDataSource.getRepository(Category)
     const relateIds: number[] | never[] = extractNumbersFromString(category_id)
     const relativeField = "categories"
@@ -85,12 +82,12 @@ export const Import_attribute = async (req: any, res: Response) => {
       };
 
       const csvData: any = await parseCSVFile(req.files, options);
-      let attributeTable: any = new Attribute(); 
+      let attributeTable: any = new Attribute();
       const keysToKeep: any = ["category_id", 'name', 'is_require', 'is_field', "status", "position", "isRelation"]
       const relativeRepo = AppDataSource.getRepository(Category)
       const relativeField = "categories"
-      const RelationOption: RelationOptionSchema = { isRelation: false, relativeRepo, relateIds:"", relativeField }
-  
+      const RelationOption: RelationOptionSchema = { isRelation: false, relativeRepo, relateIds: "", relativeField }
+
       AddMultipalRecord(attributeRepo, attributeTable, res, messageData.ATTRIBUTE_ADD_SUCCESSFULL, csvData, Attribute, keysToKeep, RelationOption, {})
     }
   } catch (error) {
